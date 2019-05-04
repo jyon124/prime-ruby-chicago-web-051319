5cfafb5f622871739b3b1b4855bbbfe988ae3b8d
Jooyon Won <jyon1204@gmail.com>
2019-05-04 20:15:06 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,5 +1,7 @@
-# Add  code here!
-def prime?(numbers)
-if Prime.each(numbers) do |numbers|
-p prime
-end
+def prime?(number_to_test)
+  return false if number_to_test < 2
+  (2..Math.sqrt(number_to_test)).each do |num|
+    return false if number_to_test % num == 0
+  end
+  true
+end 
